894b0b00ac0fbec27db266e204ed5a00f918e48c
kenjiszk <kenji.suzuki.1984@gmail.com>
2019-04-28 16:22:14 +0900

    add uniqu index on uuid

--- db/migrate/20190428043715_create_users.rb
+++ db/migrate/20190428043715_create_users.rb
@@ -5,5 +5,6 @@ class CreateUsers < ActiveRecord::Migration[5.2]
 
       t.timestamps
     end
+    add_index :users, :uuid, unique: true
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 2019_04_28_050433) do
     t.string "uuid", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["uuid"], name: "index_users_on_uuid", unique: true
   end
 
   add_foreign_key "challenge_details", "challenges"
