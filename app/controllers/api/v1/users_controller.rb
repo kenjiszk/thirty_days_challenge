401915094a95fd964ef51159bf3843a501dd14cf
kenjiszk <kenji.suzuki.1984@gmail.com>
2019-04-28 16:08:23 +0900

    add user controller

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,23 @@
+module Api
+  module V1
+    class UsersController < ApplicationController
+      protect_from_forgery with: :null_session
+      def show
+        user = User.find_by!(uuid: params[:uuid])
+        render json: {user: user}
+      end
+
+      def create
+        user = User.new(user_params)
+        user.save
+        render json: { status: 'SUCCESS', data: params }
+      end
+
+      private
+
+      def user_params
+        params.permit(:uuid)
+      end
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 2019_04_28_050433) do
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "uuid", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
